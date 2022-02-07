037e540cc6779a29a76afc51b7b6cc77160c6095
jung su young <j9972@naver.com>
2022-02-07 22:26:35 +0900

    show post on browser

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Home() {
-  return <div>make a post</div>;
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setPostList(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {postList.map((val, key) => {
+        return (
+          <div key={key} className="post">
+            <div className="title">{val.title}</div>
+            <div className="body">{val.description}</div>
+            <div className="footer">{val.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Home;
--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Posts() {
-  return <div></div>;
+  return <div>heelo</div>;
 }
 
 export default Posts;
